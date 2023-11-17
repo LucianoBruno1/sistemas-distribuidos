1016a357b95f7f10c0bc24d8d20abd1d23d52a16
Luciano Bruno <luciaanobruno@gmail.com>
2023-11-16 22:36:27 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask.app import Flask
+from flask_socketio import SocketIO, emit
+from flask.templating import render_template
+
+
+app = Flask(__name__)
+io = SocketIO(app)
+
+@app.route("/")
+def home():
+    return render_template("chat.html")
+
+@io.on('sendMessage')
+def send_message_handler(msg):
+    emit('getMessage', msg, json=True)
+
+
+if __name__ == '__main__':
+    io.run(app, debug=True)
