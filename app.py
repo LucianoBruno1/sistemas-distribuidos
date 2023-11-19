990ac7ed804ee1c9d10c5f486262e358396d9371
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-11-18 21:33:33 -0300

    update

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask.app import Flask
-from flask_socketio import SocketIO, emit, send
-from flask.templating import render_template
-
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 
 app = Flask(__name__)
 io = SocketIO(app)
 
-messages = []
+rooms = {}  # Dicionário para mapear salas e mensagens
 
 @app.route("/")
 def home():
@@ -14,13 +12,22 @@ def home():
 
 @io.on('sendMessage')
 def send_message_handler(msg):
+    room = msg.get('room', None)
+    messages = rooms.get(room, [])  # Obter mensagens da sala
     messages.append(msg)
-    emit('getMessage', msg, broadcast=True)
+    rooms[room] = messages
+    emit('getMessage', msg, broadcast=True, room=room)
 
+@io.on('join')
+def join_handler(data):
+    room = data['room']
+    join_room(room)
+    emit('message', rooms.get(room, []), room=room)
 
-@io.on('message')
-def message_handler(msg):
-    send(messages)
+@io.on('leave')
+def leave_handler(data):
+    room = data['room']
+    leave_room(room)
 
 if __name__ == '__main__':
     io.run(app, debug=True)
